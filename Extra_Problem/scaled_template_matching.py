21aa7f7153174befad3875ec12a20f58fdd60784
Toni Divic <tdivic@stanford.edu>
2020-10-22 15:20:02 -0700

    image pyramids implemented, not fully working

--- Extra_Problem/scaled_template_matching.py
+++ Extra_Problem/scaled_template_matching.py
@@ -4,7 +4,6 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-
 def template_match(template, image,
                    num_upscales=2, num_downscales=3,
                    detection_threshold=0.93):
@@ -20,7 +19,65 @@ def template_match(template, image,
         matches: A list of (top-left y, top-left x, bounding box height, bounding box width) tuples for each match's bounding box.
     """
     ########## Code starts here ##########
-    raise NotImplementedError("Implement me!")
+    # create empty matches array
+    matches = []
+
+    # first get the matches for the original image
+    new_matches = problem4_template_matching(template, image, threshold=detection_threshold)
+    matches.extend(new_matches)
+
+    # then loop through the upscales
+    upscaled_image = image
+    for i in range(num_upscales):
+        # upscale the last upscaled image and save it as the new upscaled image
+        upscaled_image = cv2.pyrUp(upscaled_image)
+
+        # template match on this new image
+        new_matches = problem4_template_matching(template, upscaled_image, threshold=detection_threshold)
+
+        # append the new matches to our list of matches
+        matches.extend(new_matches)
+
+    # finally, loop through the downscales
+    downscaled_image = image
+    for i in range(num_downscales):
+        # downscale the image
+        downscaled_image = cv2.pyrDown(downscaled_image)
+
+        # template match
+        new_matches = problem4_template_matching(template, downscaled_image, threshold=detection_threshold)
+
+        # append to our list
+        matches.extend(new_matches)
+
+    return matches
+    ########## Code ends here ##########
+
+
+def problem4_template_matching(template, image, threshold=0.999):
+    """
+    Input
+        template: A (k, ell, c)-shaped ndarray containing the k x ell template (with c channels).
+        image: An (m, n, c)-shaped ndarray containing the m x n image (with c channels).
+        threshold: Minimum normalized cross-correlation value to be considered a match.
+
+    Returns
+        matches: A list of (top-left y, top-left x, bounding box height, bounding box width) tuples for each match's bounding box.
+    """
+    ########## Code starts here ##########
+    results = cv2.matchTemplate(image, template, method=cv2.TM_CCORR_NORMED)
+
+    # find all points in the resulting grid that exceed our matching threshold
+    match_corners = np.argwhere(results > threshold)
+
+    # for all the match corners, create the bounding box and add it to our matches
+    matches = []
+    width = np.shape(template)[1]
+    height = np.shape(template)[0]
+    for match_corner in match_corners:
+        matches.append((match_corner[0], match_corner[1], height, width))
+
+    return matches
     ########## Code ends here ##########
 
 
